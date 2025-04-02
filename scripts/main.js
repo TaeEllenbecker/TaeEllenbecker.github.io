59fa35a6376efc7bc016f6f591350c8af5795258
TaeEllenbecker <ellenbeckertae@gmail.com>
2025-04-01 21:08:55 -0500

    Update Project Cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,2 @@
-const myHeading = document.querySelector("h1");
 
-myHeading.addEventListener("click", () => {
-  const mytextcontent = myHeading.textContent;
-  if (mytextcontent === "dang") {
-    myHeading.textContent = "dangerous";
-  } else{
-    myHeading.textContent = "dang";
-  }
-});
 
