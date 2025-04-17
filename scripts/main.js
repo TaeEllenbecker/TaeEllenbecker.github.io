f723a613f583ddf1200712fa96467606199deaaf
Tae Ellenbecker <ellenbeckertae@gmail.com>
2025-04-16 20:58:09 -0500

    Added validation checking using JS for the form inputs

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
+const form = document.getElementById('form');
+const formName = document.getElementById('formName');
+const formEmail = document.getElementById('email');
+const formMessage = document.getElementById('message');
 
+let errors = [];
+
+formName.addEventListener('blur', () => {
+    if (formName.value.trim() === '') {
+        formName.nextElementSibling.textContent = 'Please type your name';
+    } else {
+        formName.nextElementSibling.textContent = '';
+    }
+});
+
+formEmail.addEventListener('blur', () => {
+    if (formEmail.value.trim() === '') {
+        formEmail.nextElementSibling.textContent = 'Please type your email';
+    } 
+    else if( !((formEmail.value.trim()).includes('@') ) )  {
+        formEmail.nextElementSibling.textContent = '';
+        formEmail.nextElementSibling.textContent = 'Please type a valid email';
+    }
+    else{
+        formEmail.nextElementSibling.textContent = '';
+    }
+});
+
+formMessage.addEventListener('blur', () => {
+    if (formMessage.value.trim() === '') {
+        formMessage.nextElementSibling.textContent = 'Please type your message';
+    } else {
+        formMessage.nextElementSibling.textContent = '';
+    }
+});
